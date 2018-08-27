82434cabe2d540d2f746ee8321d8935f37d0dea7
megzimo <40956115+megzimo@users.noreply.github.com>
2018-08-27 12:47:41 -0500

    js pseudo code written and some styling/background image added

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This game gives players 15 seconds to listen to song clips and correctly guess t
 
 Upon answer choice, the correct choice will be revealed before moving on to the next question. If you run out of time, the gamme will automatically end, so be sure to make a choice before time runs out, even if you're not 100% sure.
 
-
+<br>
 
 Technology used in this project:
 _________________________________________________________________________________________________________________________
---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    // create object containing questions/answers and other key/value pairs
+
+    // create variables
+    let questions = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10];
+    let currentQuestion = 0;
+    let userGuess = '';
+    let correctAnswers = 0;
+    let wrongAnswers = 0;
+    let unanswered = 0;
+    let userChoise ='';
+    let correctAnswer='';
+    let timeLeft = 20;
+    let counter = 0;
+
+    // 'start' button function
+
+    // display questions/start timer function
+
+    // next question function
+
+    // check timer function
+
+    // reset function
+
+}); // ENDS document.ready(fn())
